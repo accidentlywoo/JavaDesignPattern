d64340ce7a679b4f364c1efb12f2abd45dd4cedb
accidentlywoo <accidentlywoo@gmail.com>
2020-03-11 18:54:33 +0900

    Adapter patter

---
+++ src/main/java/gof/design/pattern/adapter/Adapter.java
@@ -0,0 +1,9 @@
+package gof.design.pattern.adapter;
+
+public interface Adapter {
+
+    // 추가되는 원하는 기능
+    Float twiceOf(Float f);
+    // 추가되는 원하는 기능
+    Float halfOf(Float f);
+}
---
+++ src/main/java/gof/design/pattern/adapter/Math.java
@@ -0,0 +1,11 @@
+package gof.design.pattern.adapter;
+
+public class Math {
+
+    public static double towTime(double num){return num*2;}
+
+    public static double half(double num){return num/2;}
+
+    //강화된 알고리즘
+    public static Double doubled(Double d){return d*2;}
+}
